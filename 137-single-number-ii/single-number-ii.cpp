eeda56917feb96658d580f845476fc0225f4a18b
solosurfer-37 <vanshk.0037@gmail.com>
2026-07-18 17:22:23 +0530

    Time: 3 ms (31.34%) | Memory: 14 MB (17.78%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int  ,int > aloo ;
+        map<int  ,int > aloo ;
         for(int i = 0 ; i < nums.size() ; i++ ){
             aloo[nums[i]] ++ ;
         }
