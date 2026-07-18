8013410870178b66448e5eb2b03c8fac8b8db174
solosurfer-37 <vanshk.0037@gmail.com>
2026-07-18 17:22:07 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (10.78%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int  ,int > aloo ;
+        for(int i = 0 ; i < nums.size() ; i++ ){
+            aloo[nums[i]] ++ ;
+        }
+        for(auto it = aloo.begin() ; it != aloo.end() ; it++ ){
+            if(it->second < 3 ){
+                return it->first ;
+            }
+        }
+        return 0 ;
+    }
+};
